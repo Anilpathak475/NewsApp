80263495c233136ba878cfc827d9c77dad4570fc
Anil Pathak <anil@savedroid.de>
2020-04-03 15:01:13 +0200

    Initial commit

---
+++ app/src/main/java/com/anil/newapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.anil.newapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anil.newapp.R
+
+class  MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
